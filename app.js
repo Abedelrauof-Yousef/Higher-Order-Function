3a1bdf1fd2501ec1477cab178e55bebb1d476db5
Abedelrauof <abedelrauofyousef@gmail.com>
2024-06-22 23:53:49 +0300

    higher order function

---
+++ app.js
@@ -0,0 +1,47 @@
+// Ex1 using for
+// let arr = [1, 2, 3, 4, 5];
+// function adding(){
+//     for(let i = 0; i < arr.length; i++){
+//         arr[i] = (Math.pow(2, arr[i]));
+//     }
+//     console.log(arr);
+// }
+// adding();
+
+
+
+
+
+// Ex1 using forEach
+// let arr = [1, 2, 3, 4, 5];
+// let adding = arr.forEach(function(arr){
+//     return Math.pow(2, arr);
+// });
+// console.log(adding);
+
+
+
+
+
+
+
+
+
+
+// Ex1 using map
+// let arr = [1, 2, 3, 4, 5];
+// let selfAdd = arr.map(function(element, index, array) {
+//     return Math.pow(2, element);
+// });
+// console.log(selfAdd)
+
+
+
+
+
+
+// Ex3
+// let names = ["Rawan", "Wesam", "Hind", "Muhammad", "Esraa", "Dareen"];
+// names.forEach(function(names){
+//     console.log(names);
+// });
