241356ab995c72a6f419e6087030364ce4d68844
Abedelrauof <abedelrauofyousef@gmail.com>
2024-06-23 00:23:20 +0300

    ex4 added

--- app.js
+++ app.js
@@ -45,3 +45,25 @@
 // names.forEach(function(names){
 //     console.log(names);
 // });
+
+
+
+
+
+
+// Ex4
+// function fizzbuzz() {
+//     for (let i = 1; i <= 100; i++) {
+//         if (i % 3 == 0 && i % 5 == 0) {
+//             console.log("FizzBuzz");
+//         } else if (i % 3 == 0) {
+//             console.log("Fizz");
+//         } else if (i % 5 == 0) {
+//             console.log("Buzz");
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// fizzbuzz();
